0292f0c3a8988a08601d3b6d9082424d63462437
blue-w-potato <wame20080418@gmail.com>
2024-05-15 07:01:40 +0800

    f

--- b/b026.py
+++ b/b026.py
@@ -1,6 +1,8 @@
-ru = [101] + list(map(int,input().split()))
+a = [101] + list(map(int,input().split()))
 k = 0
-for i in range(1, len(ru)):
-    if min(ru[:i]) < max(ru[i:]) and max(ru[i:]) - min(ru[:i]) > k and max(ru[i:]) + min(ru[:i]) > 0:
-        k = max(ru[i:]) - min(ru[:i])
+for i in range(1, len(a)):
+    ma = max(a[i:])
+    mi = min(a[:i])
+    if (mi < ma) and (ma - mi > k) and (abs(ma) >= abs(mi)):
+        k = ma - mi
 print(k)
