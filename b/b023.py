1a04e5725ab46499832a65505e4d48826eefc95f
blue-w-potato <wame20080418@gmail.com>
2024-05-16 19:00:56 +0800

    river

--- b/b023.py
+++ b/b023.py
@@ -1,1 +1,8 @@
-#修但幾勒
+def river( n, x, y, z ):
+    if n == 1:
+        print( f'Ring 1 from {x} to {y}' )
+        return
+    river( n-1, x, z, y )
+    print( f'Ring {n} from {x} to {y}' )
+    river( n-1, z, y, x )
+river(int(input()), 1, 3, 2)
